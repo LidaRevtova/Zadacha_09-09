c4f2ac1bf33cb9ec4b9d4134ad7722cbbba8d16a
LidaRevtova <lida.revtova@yandex.ru>
2020-09-09 10:16:55 +0700

    end

--- main.py
+++ main.py
@@ -1,11 +1,15 @@
 def decorator(func):
     """"""
+
     import csv
     import functools
+
     @functools.wraps(func)
     def wrapped(*args, **kwargs):
         """"""
+
         result = func(*args, **kwargs)
         result = csv.writer(result)
         return result
-    return wrapped()
+
+    return wrapped
